6c0623d00036eea940b82d13c18f643c56874ecd
Sharmila Korada <sharmilakorada23@gmail.com>
2026-07-28 17:20:13 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0]*numCourses
+
+        for edge in prerequisites:
+            adj[edge[1]].append(edge[0])
+            indegree[edge[0]] += 1
+        
+        q = deque()
+        res = []
+        
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            top = q.popleft()
+            res.append(top)
+            for neigh in adj[top]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        
+        if len(res) == numCourses:
+            return True
+        return False
